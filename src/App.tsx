1ce3298e134223205d5e8224d0ff34a4e6d67daa
ngimdock <ngimdockzemfack@gmail.com>
2023-04-07 21:32:44 +0100

    Implementing the frontend part with mock data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { UserDisplay } from "./components/user-display";
+import { User } from "./types";
+
 function App() {
+  const users: User[] = [
+    {
+      name: "ngimdock",
+      messages: [{ body: "Hello world !" }, { body: "Je suis dev" }],
+    },
+  ];
   return (
-    <h2 className="text-3xl font-bold underline text-blue-500">
-      Hello world !
-    </h2>
+    <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gap-y-12 overflow-scroll">
+      {users.map((user, i) => (
+        <UserDisplay user={user} key={i} />
+      ))}
+    </div>
   );
 }
 
