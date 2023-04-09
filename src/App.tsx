0bd68925ce2ff978a8f48dfe7c92d1e69c51bb02
ngimdock <ngimdockzemfack@gmail.com>
2023-04-09 09:37:05 +0100

    cleanup the project

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,9 @@ import { useQuery } from "urql";
 import { GetUsersDocument } from "./graphql/generate";
 
 function App() {
-  const users: User[] = [
-    {
-      name: "ngimdock",
-      messages: [{ body: "Hello world !" }, { body: "Je suis dev" }],
-    },
-  ];
-
   const [result] = useQuery({
     query: GetUsersDocument,
   });
-
-  console.log({ dataUsers: result.data?.users });
 
   return (
     <div className="flex flex-col items-center justify-center w-full p-4 bg-zinc-800 gap-y-12">
